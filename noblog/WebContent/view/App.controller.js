9dd0500dd245fb81b98efa7928602f76355a3e0e
Patrick Reif <patrick.reif@inf.h-brs.de>
2015-12-28 19:21:37 +0100

    Implemented register page for login popup

--- noblog/WebContent/view/App.controller.js
+++ noblog/WebContent/view/App.controller.js
@@ -1,31 +1,57 @@
 jQuery.sap.require("util.Formatter");
 jQuery.sap.require("sap.m.MessageBox");
 jQuery.sap.require("model.dao.UserDAO");
+jQuery.sap.require("model.dao.UserDTO");
+jQuery.sap.require("model.dao.User");
 
 sap.ui.controller("view.App", {
 	
-	_oLoginPopover : null,
+	// Login Popover
+	_oLoginPopover 				: null,
+	_oLoginPopoverNavContainer 	: null,
 	
 	onInit: function() {
 		this._oComponent 	= this.getOwnerComponent();
 		this._oView			= this.getView();
-		this._oUserDAO		= com.team6.noblog.model.dao.UserDAO.getInstance();
 		
-		this._oLoginPopover = sap.ui.xmlfragment("view.fragment.LoginPopover", this);
-		this._oView.addDependent(this._oLoginPopover);
-		
-		// attach additional model to popover to prevent id conflicts
-		var oModelPopover	= new sap.ui.model.json.JSONModel();
-		oModelPopover.setData({
-			username : "meinuser",
-			password : "test"
-		});
-		this._oLoginPopover.setModel(oModelPopover, "login");
-		this._oLoginPopover.bindElement("login>/");
+		this.initLoginPopover();
 	},
 	
 	// ###########################################################################
 	// ### LOGIN POPOVER
+	
+	initLoginPopover : function() {
+		
+		// Init LoginPopover and all of its dependencies
+		this._oUserDAO					= com.team6.noblog.model.dao.UserDAO.getInstance();
+		this._oLoginPopover 			= sap.ui.xmlfragment("view.fragment.LoginPopover", this);
+		this._oLoginPopoverNavContainer = sap.ui.getCore().byId("id_loginpopover_navcontainer");
+		
+		// Create model for popover to get data
+		this._resetLoginPopoverModel();
+		
+		// set model and attach events and lifecycle
+		this._oLoginPopover.attachAfterClose(this.onAfterLoginPopoverClosed, this);
+		this._oView.addDependent(this._oLoginPopover);
+	},
+	
+	_resetLoginPopoverModel : function() {
+		var oModelPopover	= new sap.ui.model.json.JSONModel({
+			login : {
+				username : "meinuser",
+				password : "password"
+			},
+			register : {
+				username 	: "",
+				password 	: "",
+				prename		: "",
+				surname		: "",
+				email		: ""
+			}
+		});
+		
+		this._oLoginPopover.setModel(oModelPopover, "loginpopover");
+	},
 	
 	showLoginPopover : function(oTarget) {
 		jQuery.sap.delayedCall(0, this, function () {
@@ -35,10 +61,10 @@ sap.ui.controller("view.App", {
 	
 	handleButtonLoginPressed: function(oEvent) {
 		var oSource 	= oEvent.getSource();
-		var oModel		= oSource.getModel("login");
+		var oModel		= oSource.getModel("loginpopover");
 		
-		var sUsername 	= oModel.getProperty("/username");
-		var sPassword	= oModel.getProperty("/password");
+		var sUsername 	= oModel.getProperty("/login/username");
+		var sPassword	= oModel.getProperty("/login/password");
 		
 		this._oUserDAO.tryLogin(sUsername, sPassword, function(bSucceeded) {
 			if( bSucceeded ) {
@@ -46,11 +72,39 @@ sap.ui.controller("view.App", {
 			} else {
 				sap.m.MessageBox.alert("Login failed. Please check your username and password");
 			}
+			
+			this._oLoginPopover.close();
+			
 		}.bind(this));
 	},
 	
+	handleLinkRegisterClicked: function(oEvent) {
+		this._oLoginPopoverNavContainer.to("id_loginpopover_navcontainer_register");
+	},
+	
+	handleButtonBackPressed: function(oEvent) {
+		this._oLoginPopoverNavContainer.back();
+	},
+	
 	handleButtonRegisterPressed: function(oEvent) {
+		var oSource 	= oEvent.getSource();
+		var oModel		= oSource.getModel("loginpopover");
 		
+		var sUsername 	= oModel.getProperty("/register/username");
+		var sPassword	= oModel.getProperty("/register/password");
+		var sPrename 	= oModel.getProperty("/register/prename");
+		var sSurname	= oModel.getProperty("/register/surname");
+		var sEmail 		= oModel.getProperty("/register/email");
+		
+		var oUser = new com.team6.noblog.model.dao.User(sUsername, sPassword, sPrename, sSurname, sEmail);
+		console.log(oUser);
+		
+		this._oLoginPopover.close();
+	},
+	
+	onAfterLoginPopoverClosed: function(oEvent) {
+		this._resetLoginPopoverModel();
+		this._oLoginPopoverNavContainer.backToTop();
 	}
 	
 	// ###########################################################################
